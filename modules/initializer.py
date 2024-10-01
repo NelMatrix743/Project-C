fbb9af232e0f1dd4c2d65c4f25f035b17dee1172
Nelmatrix <nelmatrix155@gmail.com>
2024-10-01 18:04:50 +0000

    Included the welcome message into a Panel for aestetic purposes.

--- modules/initializer.py
+++ modules/initializer.py
@@ -33,14 +33,19 @@ class Initializer():
 
     def display_welcome_message() -> None:
         welcome_message: str = """
-[bold blink]WELCOME TO PROJECT-C[/]
-[italic]Project-C is your go-to CLI tool for creating and managing Python projects effortlessly from the command line. 
-With Project-C, you can customize your project setup by selecting the files and directories you want to include, like README files, __init__ files, test directories, and more. 
-It also offers built-in support for setting up git repositories and Python virtual environments automatically—unless you prefer to handle those yourself.
+[italic bright_yellow]Project-C is your go-to CLI tool for creating and managing Python projects effortlessly from the command line.
+With Project-C, you can customize your project setup by selecting the files and directories you want to include, like README files, __init__ files, test directories, and more.
+It also offers built-in support for setting up git repositories and Python virtual environments automatically — unless you prefer to handle those yourself.
 Whether you're starting a new project, managing existing ones, or cleaning up, Project-C is here to streamline the process. Happy coding![/]
         """
+        welcome_message_box: Panel = Panel(
+            welcome_message,
+            title = "[bold blink]WELCOME TO PROJECT-C[/]",
+            border_style = "bold yellow",
+            padding = (0, 2)
+        )
 
-        Initializer.terminalController.print(welcome_message)
+        Initializer.terminalController.print(welcome_message_box)
         # with Console.status():
         #     #TODO: retrieve project-c initialisation state from the config.db file
 
