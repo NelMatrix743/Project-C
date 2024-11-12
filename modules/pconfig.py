2cdf6a6dea742fdafba1b4045340387fe8f6a0ec
Nelmatrix <nelmatrix155@gmail.com>
2024-11-12 15:52:15 +0000

    I added some methods to the ProjectConfigurator class that deal with getting and setting the project reservoir path entered by end user(s).

--- modules/pconfig.py
+++ modules/pconfig.py
@@ -1,16 +1,44 @@
 ###############################################################################
 # Programmer.name = Nelson Chidi                                              #
 # Programmer.nick_name = Nelmatrix                                            #
-# Programmer.GitHub.username = NelMatrix743                                   #
+# Programmer.GitHub.user_name = NelMatrix743                                  #
 # Programmer.GitHub.url = https://github.com/NelMatrix743                     #
 ###############################################################################
 
 from databases import ConfigurationDatabase
 from databases import ProjectDatabase
+from pathlib import Path
+import yaml
 
 
 class ProjectConfigurator():
-    pass
+    
+    # Project-C related configuration methods:
+
+    def set_reservoir_path(input_path: Path) -> None:    
+        PROJECT_C_CONFIG: dict[str, any] = ConfigurationDatabase.load_configuration()
+        PROJECT_C_CONFIG["Reservoir Path"] = str(input_path)
+        ConfigurationDatabase.save_configuration_changes(PROJECT_C_CONFIG)
+
+
+    def get_reservoir_path() -> str | None:
+        PROJECT_C_CONFIG: dict[str, any] = ConfigurationDatabase.load_configuration()
+        return PROJECT_C_CONFIG["Reservoir Path"]
+
+
+    # User projects releated configuration methods:
+
+    # Next method starts from this particular line
+
+
+
+if __name__ == "__main__":
+
+    # Test and Debug Run
+
+    user_path: Path = Path("/home/n3lm4tr1x/project_s/Python_Programs/projectc")
+    ProjectConfigurator.set_reservoir_path(user_path)
+    print(f"User entered the following path: {ProjectConfigurator.get_reservoir_path()}")
 
 
 # end of source code
