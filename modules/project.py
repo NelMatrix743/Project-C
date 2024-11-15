2053ce0a0f93215a202e17d3df2f62190cc69491
Nelmatrix <nelmatrix155@gmail.com>
2024-11-15 11:36:59 +0000

    I added a new property to the Project class called venv_name. It stores the Project-C generated name (the name inserted in the shell prompt when a venv is activated) for a project's VENV.

--- modules/project.py
+++ modules/project.py
@@ -14,14 +14,33 @@ from utils import Util
 
 class Project():
     
-    def __init__(self, input_name, description: str | None):
+    def __init__(self, input_name, description: str | None = None):
         self.raw_name: str = input_name
-        self.parsed_name: str = Util.parse_name(input_name)
-        self.p_uid: str = Util.generate_project_uid(self.parsed_name)
+        self._parsed_name: str = Util.parse_name(input_name)
+        self.p_uid: str = Util.generate_project_uid(self._parsed_name)
         self.description: str | None = description
         self.creation_datetime: dict[str, str] = Util.retrieve_datetime()
         self.status: str | None = None
         self.full_path: str = None
+        self.venv_name: str | None = None
 
-    
+
+
+    @property
+    def parsed_name(self) -> str:
+        return str.join('_', self._parsed_name)
+
+
+    def __str__(self) -> str:
+        return f"""
+Raw Name: {self.raw_name}
+Project ID: {self.p_uid}
+Parsed Name: {self.parsed_name}
+Description: {self.description}
+        """
+
+if __name__ == "__main__":
+    p = Project("Hello World", description="This is a simple Hello world program.")
+    print(p)
+
 # end of Project()
