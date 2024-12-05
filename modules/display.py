7a2b36c5983bdd46e700db45e7e4fe401c33b310
Nelmatrix <nelmatrix155@gmail.com>
2024-12-05 21:42:57 +0000

    I made some changes to the display.py module. I added the turn_icon_off flag to each message printing function to turn off the icon if not necessary.

--- modules/display.py
+++ modules/display.py
@@ -12,32 +12,32 @@ displayController: Console = Console() # Global console controller
 
 
 # Prints a message when an action is successful
-def success_msg_display(message: str, newline: int = 1) -> None: 
-    success_icon: str = "[bold bright_green]:heavy_check_mark:[/]"
+def success_msg_display(message: str, newline: int = 1, turn_icon_off=False) -> None: 
+    success_icon: str = '' if turn_icon_off else  "[bold bright_green]:heavy_check_mark:[/]"
     msg_indicator: str = '[' + "[bold bright_green]" + "SUCCESS" + "[/]" + ']'
     message = "[bright_green]" + message + "[/]"
     displayController.print(msg_indicator, message, success_icon, end=('\n'*newline))
 
 
 # Prints a message that serves as information
-def info_msg_display(message: str, newline: int = 1) -> None:
-    info_icon: str = "[bold bright_yellow](:information_source:)[/]"
+def info_msg_display(message: str, newline: int = 1, turn_icon_off=False) -> None:
+    info_icon: str = '' if turn_icon_off else  "[bold bright_yellow](:information_source:)[/]"
     msg_indicator: str = '[' + "[bold bright_yellow]" + "INFO" + "[/]" + ']'
     message: str = "[bright_yellow]" + message + "[/]"
     displayController.print(msg_indicator, message, info_icon, end=('\n'*newline))
  
 
 # Print a message that indicates a warning
-def warning_msg_display(message: str, newline: int = 1) -> None:
-    warning_icon: str = "[blink]:warning-emoji:[/]"
+def warning_msg_display(message: str, newline: int = 1, turn_icon_off=False) -> None:
+    warning_icon: str = '' if turn_icon_off else "[blink]:warning-emoji:[/]" 
     msg_indicator: str = '[' + "[bold bright_yellow]" + "WARNING" + "[/]" + ']'
     message = "[bright_yellow]" + message + "[/]"
     displayController.print(msg_indicator, message, warning_icon, end=('\n'*newline))
 
 
 # Prints a message when an action is not sucessful
-def failure_msg_display(message: str, newline: int = 1) -> None:
-    failure_icon: str = "[bold]:cross_mark:[/]" 
+def failure_msg_display(message: str, newline: int = 1, turn_icon_off=False) -> None:
+    failure_icon: str = '' if turn_icon_off else "[bold]:cross_mark:[/]" 
     msg_indicator: str = '[' + "[bold bright_red]" + "FAILURE" + "[/]" + ']'
     message = "[bright_red]" + message + "[/]"
     displayController.print(msg_indicator, message, failure_icon, end=('\n'*newline))
