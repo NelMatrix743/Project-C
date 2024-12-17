123ae74d4dfa41c03724b71b19f5d30496fe9e8f
Nelmatrix <nelmatrix155@gmail.com>
2024-12-17 23:10:56 +0000

    Hello! Its been quite some time. I have been quite busy with other stuffs. I added some changes to the ProjectCreator() class: I added a new method called create_project() which will take care of creating a project based on the type specified by a user.

--- modules/pcreator.py
+++ modules/pcreator.py
@@ -9,17 +9,39 @@ import string
 import random 
 import time 
 import venv 
-import sqlite3 as  sql 
+import sqlite3 as  sql
+from enum import Enum 
 from git import Repo 
-from .display import * 
+from display import * 
 from datetime import datetime 
-from .databases import ProjectDatabase, TemplateDatabase
+from project import Project
+from databases import ProjectDatabase, TemplateDatabase
+
+
+class ProjectType(Enum):
+    LIGHTWEIGHT: str  = "lightweight"
+    HEAVYWEIGHT: str  = "heavyweight"
+
 
 
 class ProjectCreator(): 
-
+# Create two types of project codebases:
+# Lightweight (lw) - Simple codebase structure with base files and directories like requirements.txt, README.md, test directory, etc.
+# Heavyweight (hw) - Complex, fully documented codebase structure with more files and directories such as setup.py, LICENSE.md, docs directory, etc.
+ 
     def __init__(self):
         # work on this
         pass
+    
+    
+    def create_project(project: Project, project_type: ProjectType):
+        match project_type:
+            case ProjectType.LIGHTWEIGHT:
+                pass
+            case ProjectType.HEAVYWEIGHT:
+                pass
+            case _:
+                return
+            
 
 # end of ProjectCreator()
