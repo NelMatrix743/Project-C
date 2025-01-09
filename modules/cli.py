6f9d2b0c03d9397ae99feaad2e95af7f01ffa7e4
Nelmatrix <nelmatrix155@gmail.com>
2025-01-09 16:27:37 +0000

    I added a usage_message dictionary that will contain all usage messages for the CLI interface of Project-C. I also made some slight changes to the sub parsers.

--- modules/cli.py
+++ modules/cli.py
@@ -7,28 +7,36 @@
 
 import argparse
 from argparse import ArgumentParser
+from argparse import Namespace
 
 
 HELP_MESSAGE: str = {
     "cli_inteface_help" : "A command-line utility to automate the creation and management of Python projects.", 
-    "sub_parser_help" : "List of available Project-C operations.",
+#    "sub_parser_help" : "List of available Project-C operations.",
     "init_help" : "Initialises Project-C after installation.",
     "create_help" : "Creates a new Python project.",
     "config_help" : "Used in configuring settings related to Proeject-C or a project/projects.",
     "info_help" :  "Provide information about project(s).",
-    "terminate_help" : "Delete a project or group of projects."  
+    "delete_help" : "Delete a project or group of projects."  
 }
+
+USAGE_MESSAGE: str = {
+    "cli_interface_usage" : "[projectc | pc] [subcommand] [arguments] [parameters]"
+}
+
 
 # main command parser
 cli_interface: ArgumentParser = ArgumentParser(
     prog="Project-C",
-    description=HELP_MESSAGE["cli_inteface_help"]
+    description=HELP_MESSAGE["cli_inteface_help"],
+    usage=USAGE_MESSAGE["cli_interface_usage"]
 )
 
 # subcommand parser
 sub_parsers = cli_interface.add_subparsers(
-    title="Project-C Commands", 
-    help=HELP_MESSAGE["sub_parser_help"]
+    title="Project-C Subcommands", 
+    help="", #HELP_MESSAGE["sub_parser_help"],
+    metavar=""
 )
 
 # subcommands
@@ -36,13 +44,13 @@ init_parser: ArgumentParser = sub_parsers.add_parser("init", help=HELP_MESSAGE["
 create_parser: ArgumentParser = sub_parsers.add_parser("create", help=HELP_MESSAGE["create_help"])
 config_parser: ArgumentParser = sub_parsers.add_parser("config", help=HELP_MESSAGE["config_help"])
 info_parser: ArgumentParser = sub_parsers.add_parser("info", help=HELP_MESSAGE["info_help"])
-terminate_parser: ArgumentParser = sub_parsers.add_parser("terminate", help=HELP_MESSAGE["terminate_help"])
+delete_parser: ArgumentParser = sub_parsers.add_parser("delete", help=HELP_MESSAGE["delete_help"])
 
 
 
 if __name__ == "__main__":
     
     # Write your test code here.
-    pass
+    namespace: Namespace = cli_interface.parse_args()
 
 # end of source code
