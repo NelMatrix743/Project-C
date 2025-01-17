8218ef2e25ae641d2edac694e8ee156e2dcbb090
Nelmatrix <nelmatrix155@gmail.com>
2025-01-17 19:47:23 +0000

    Implemented part of the cli.py module.

--- modules/cli.py
+++ modules/cli.py
@@ -17,13 +17,14 @@ HELP_MESSAGE: str = {
     "create_help" : "Creates a new Python project.",
     "config_help" : "Used in configuring settings related to Proeject-C or a project/projects.",
     "info_help" :  "Provide information about project(s).",
-    "delete_help" : "Delete a project or group of projects."  
+    "delete_help" : "Delete a project or group of projects.",
+    "lightweight" : "Create a lite project with minimal files and resources.",
+    "heavyweight" : "Create a fully documented project with all necessary files (LICENSE, PYPROJECT.TOML, ETC)." 
 }
 
 USAGE_MESSAGE: str = {
     "cli_interface_usage" : "[projectc | pc] [subcommand] [arguments] [parameters]"
 }
-
 
 # main command parser
 cli_interface: ArgumentParser = ArgumentParser(
@@ -33,8 +34,8 @@ cli_interface: ArgumentParser = ArgumentParser(
 )
 
 # subcommand parser
-sub_parsers = cli_interface.add_subparsers(
-    title="Project-C Subcommands", 
+sub_parsers: argparse._SubParsersAction = cli_interface.add_subparsers(
+    title="Project-C subcommands", 
     help="", #HELP_MESSAGE["sub_parser_help"],
     metavar=""
 )
@@ -46,11 +47,26 @@ config_parser: ArgumentParser = sub_parsers.add_parser("config", help=HELP_MESSA
 info_parser: ArgumentParser = sub_parsers.add_parser("info", help=HELP_MESSAGE["info_help"])
 delete_parser: ArgumentParser = sub_parsers.add_parser("delete", help=HELP_MESSAGE["delete_help"])
 
+# Defining arguments for the necessary subcommands
+
+# CREATE
+csb_option_groups: argparse._ArgumentGroup = create_parser.add_argument_group("Project Type")
+ptype_options: argparse._MutuallyExclusiveGroup = csb_option_groups.add_mutually_exclusive_group(required=True)
+ptype_options.add_argument("-lw", "--lightweight", action="store_true")
+ptype_options.add_argument("-hw", "--heavyweight", action="store_true")
+p_info: argparse._ArgumentGroup = create_parser.add_argument_group("Project Name and Description")
+p_info.add_argument("-n", "--name")
+p_info.add_argument("-d", "--description")
+
+
+
 
 
 if __name__ == "__main__":
     
     # Write your test code here.
     namespace: Namespace = cli_interface.parse_args()
+    print(f"The project name is: {namespace.name}")
+
 
 # end of source code
