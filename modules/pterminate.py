fd794086e543a1194e84e2d255b2e9ebc2b2fc90
Nelmatrix <nelmatrix155@gmail.com>
2025-02-13 22:59:22 +0000

    feat: Implement two methods for project deletion in ProjectTerminator class

--- modules/pterminate.py
+++ modules/pterminate.py
@@ -5,10 +5,38 @@
 # Programmer.GitHub.url = https://github.com/NelMatrix743                     #
 ###############################################################################
 
-from .databases import ProjectDatabase
+from databases import ProjectDatabase
+from utils import TerminationType
+from project import Project
 
 
 class ProjectTerminator():
-    pass
+    
+    def partial_termination(project_id: str) -> bool:
+        input_project: Project = ProjectDatabase.retrieve_project_data(project_id)
+        if input_project != None:
+            # Proceed with the partial delete operation
+            ProjectTerminator.delete_project_dir(input_project.full_path)
+            return True
+        return False
+    
+    
+    def total_termination(project_id: str) -> bool:
+        input_project: Project = ProjectDatabase.retrieve_project_data(project_id)
+        if input_project != None:
+            # Proceed with the complete delete operation
+            ProjectTerminator.delete_project_dir(input_project.full_path)
+            ProjectTerminator.delete_project_database_entry(project_id)
+            return True
+        return False
+
+
+    def delete_project_dir(path: str) -> bool:
+        pass 
+    
+    
+    def delete_project_database_entry(project_id: str) -> bool:
+        pass
+    
 
 # end of source code
